7eb39d6615018e67672ac6a97794f4d3e46fdfdb
Rubens Alves Lourenco <rubens.a.lourenco@beflyconecta.com.br>
2024-10-10 11:52:08 -0300

    Atualizando o app.css, criação do componente logo

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import React from 'react'
 
-import Logo from '../components/template/logo'
+import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'

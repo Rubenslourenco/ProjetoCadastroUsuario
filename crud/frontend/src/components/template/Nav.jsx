794cff11c978108eb5185cd7e5b50943b2239eb9
rubenslourenco <alvesrubens926@gmail.com>
2024-10-10 15:08:58 -0300

    Componente de navegação

--- crud/frontend/src/components/template/Nav.jsx
+++ crud/frontend/src/components/template/Nav.jsx
@@ -2,6 +2,13 @@ import './Nav.css'
 import React from 'react'
 
 export default props =>
-    <aside>
-        Navegação
+    <aside className='menu-area'>
+        <nav className='menu'>
+            <a href="#/">
+            <i className='fa fa-home'></i>Inicio
+            </a>
+            <a href="#users">
+            <i className='fa fa-users'></i>Usuarios
+            </a>
+        </nav>
     </aside>
